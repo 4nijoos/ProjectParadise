7f061cf369ea6ebf152bc27137072688205eb345
zeltina99 <minec369@outlook.com>
2026-02-11 11:02:02 +0900

    Feat: 편성에서 캐릭터와 유닛 슬롯을 관리할 패널 설계
    ## 작업 개요
    - 메인1, 서브2, 유닛5 개의 슬롯을 관리할 패널을 설계함

--- Source/Paradise/Public/UI/Widgets/Squad/ParadiseSquadFormationWidget.h
+++ Source/Paradise/Public/UI/Widgets/Squad/ParadiseSquadFormationWidget.h
@@ -12,6 +12,9 @@ class UWidgetSwitcher;
 class UParadiseSquadSlot;
 #pragma endregion 전방 선언
 
+/** @brief 슬롯 선택 시 인덱스 전달 (0:Main, 1:Sub1...) */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnFormationSlotSelected, int32, SlotIndex);
+
 /**
  * @class UParadiseSquadFormationWidget
  * @brief 좌측 상단 편성 슬롯(메인/서브/유닛)을 관리하는 뷰 위젯
@@ -21,4 +24,65 @@ class PARADISE_API UParadiseSquadFormationWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+#pragma region 공개 함수
+public:
+	/**
+	 * @brief 현재 탭 상태에 따라 좌측 편성 패널의 모드를 전환합니다.
+	 * @details WidgetSwitcher를 제어하여 '캐릭터 편성(3슬롯)' 화면과 '유닛 덱 편성(5슬롯)' 화면을 교체합니다.
+	 * @param bIsUnitMode true인 경우 유닛 덱 모드(Unit Deck)로, false인 경우 캐릭터 편성 모드(Squad)로 설정합니다.
+	 */
+	void SetFormationMode(bool bIsUnitMode);
+
+	/**
+	 * @brief 지정된 인덱스를 가진 편성 슬롯의 표시 데이터를 갱신합니다.
+	 * @details 메인/서브 캐릭터 혹은 유닛 슬롯의 아이콘, 레벨, 등급 정보를 업데이트합니다.
+	 * @param SlotIndex 업데이트할 대상 슬롯의 고유 인덱스 (예: 0=Main, 1=Sub1, 3=Unit1).
+	 * @param Data 표시에 필요한 데이터 모델 (View Model). ID가 유효하지 않으면 빈 슬롯으로 처리됩니다.
+	 */
+	void UpdateSlot(int32 SlotIndex, const FSquadItemUIData& Data);
+
+	/**
+	 * @brief 특정 슬롯을 시각적으로 강조(Highlight)하고, 나머지 슬롯의 강조는 해제합니다.
+	 * @details 사용자의 선택을 시각적으로 피드백하기 위해 테두리 이미지를 켜거나 끕니다. (Radio Button 동작)
+	 * @param SlotIndex 선택된(강조할) 슬롯의 인덱스.
+	 */
+	void HighlightSlot(int32 SlotIndex);
+#pragma endregion 공개 함수
+
+#pragma region 생명주기
+protected:
+	virtual void NativeConstruct() override;
+#pragma endregion 생명주기
+
+#pragma region UI 바인딩
+protected:
+	/** @brief 모드 전환 (0: 캐릭터 편성, 1: 유닛 덱 편성) */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> Switcher_Mode = nullptr;
+
+	// --- 캐릭터 슬롯 ---
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Main = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Sub1 = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Sub2 = nullptr;
+
+	// --- 유닛 슬롯 (5개) ---
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Unit1 = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Unit2 = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Unit3 = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Unit4 = nullptr;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UParadiseSquadSlot> Slot_Unit5 = nullptr;
+#pragma endregion UI 바인딩
+
+#pragma region 내부 로직
+private:
+	/** @brief 슬롯 클릭 시 호출되는 핸들러 */
+	UFUNCTION()
+	void HandleSlotClick(int32 SlotIndex);
+#pragma endregion 내부 로직
+
+#pragma region 델리게이트
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnFormationSlotSelected OnSlotSelected;
+#pragma endregion 델리게이트
 };
